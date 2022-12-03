762872074311a2905e4ab404cff3e015de2ca841
Oscar Mojica <o_mojical@hotmail.com>
2022-12-02 21:42:03 -0300

    small refactoring

--- 02/day2.f90
+++ 02/day2.f90
@@ -32,32 +32,15 @@ integer :: score
     select case (line)
     case ('A Y', 'B Z', 'C X')
         score = 6
-        if (line(3:3) == 'X') then
-            score = score + 1
-        else if (line(3:3) == 'Y') then
-            score = score + 2
-        else
-            score = score + 3
-        endif
+        score = score + conditional1(line(3:3))
     case ('A X', 'B Y', 'C Z')
         score = 3
-        if (line(3:3) == 'X') then
-            score = score + 1
-        else if (line(3:3) == 'Y') then
-            score = score + 2
-        else
-            score = score + 3
-        endif
+        score = score + conditional1(line(3:3))
     case default
         score = 0
-        if (line(3:3) == 'X') then
-            score = score + 1
-        else if (line(3:3) == 'Y') then
-            score = score + 2
-        else
-            score = score + 3
-        endif
+        score = score + conditional1(line(3:3))
     end select
+
 end function original_strategy
 
 elemental function modified_strategy(line) result(score)
@@ -68,35 +51,46 @@ integer :: score
 ! shape scores => rock = 1, paper = 2, scissors = 3
 ! round score => win = 6, draw = 3, lose = 0
 
-    select case (line(3:3))
-    case ('Y') ! draw
+    select case (line)
+    case ('A X', 'C Y', 'B Z')
         score = 3
-        if (line(1:1) == 'A') then
-            score = score + 1
-        else if (line(1:1) == 'B') then
-            score = score + 2
-        else
-            score = score + 3
-        endif
-    case ('Z') ! win
-        score = 6
-        if (line(1:1) == 'A') then
-            score = score + 2
-        else if (line(1:1) == 'B') then
-            score = score + 3
-        else
-            score = score + 1
-        endif
-    case default ! lose
-        score = 0
-        if (line(1:1) == 'A') then
-            score = score + 3
-        else if (line(1:1) == 'B') then
-            score = score + 1
-        else
-            score = score + 2
-        endif
+        score = score + conditional2(line(3:3))
+    case ('C X', 'B Y', 'A Z')
+        score = 2
+        score = score + conditional2(line(3:3))
+    case default
+        score = 1
+        score = score + conditional2(line(3:3))
     end select
+
 end function modified_strategy
+
+elemental function conditional1(line) result(score)
+character(1), intent(in) :: line
+integer ::  score
+
+if (line == 'X') then
+    score = 1
+else if (line == 'Y') then
+    score = 2
+else
+    score = 3
+endif
+
+end function conditional1
+
+elemental function conditional2(line) result(score)
+character(1), intent(in) :: line
+integer ::  score
+
+if (line == 'X') then ! X => lose
+    score = 0
+else if (line == 'Y') then ! Y => draw
+    score = 3
+else ! Z => win
+    score = 6 
+endif
+
+end function conditional2
 
 end program day2
